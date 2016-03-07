d7be443e480e18f610a9cc0f95df8f07fd44ead9
root <root@internalapps-ipos.caratlane-server.internal>
2016-03-07 14:40:35 +0530

    Installing a better restart script

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,24 @@
-#! /bin/bash
+#!/bin/sh
 
-git pull --rebase
+cd /opt/code-base/internal-server-1/taskflow/
+git remote update
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
 
+if [ $LOCAL = $REMOTE ]; then
+      echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+      echo "Need to pull"
+      git stash save && git pull --rebase && git stash pop
+      NEWLOCAL=$(git rev-parse @)
+      if [ $NEWLOCAL = $REMOTE ]; then
+        echo "Pull successfull"
+        service nginx restart
+        service php5-fpm restart
+      fi
+elif [ $REMOTE = $BASE ]; then
+      echo "Need to push"
+else
+      echo "Diverged"
+fi
